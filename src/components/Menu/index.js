1f67574b6eb2c94f4305348e73894098beaac471
dpaffrath <dpaffrath@users.noreply.github.com>
2020-07-28 21:06:56 -0300

    Add files via upload
    Aula 2

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css'
-import ButtonLink from './components/ButtonLink';
 import Button from '../Button';
 
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
-                <img className="Logo" src={Logo}/>
-            </a>
+            <Link to="/">
+                <img className="Logo" src={Logo} alt="DcylFlix logo"/>
+            </Link>
 
-            <Button className="ButtonLink" href="/">
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo vídeo
             </Button>
         </nav>
-    )
+    );
 }
 
 export default Menu;
